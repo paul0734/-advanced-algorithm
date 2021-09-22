a5b08706fbf9f9cca43c8f0f7014c725d1394c57
paul0734 <56014949+paul0734@users.noreply.github.com>
2021-09-22 09:33:44 +0900

    Create 문제2.java
    02984 / 576
    가장 큰수가 되도록 곱하기 혹은 더하기 (0과 1은 더하기 나머진 곱하기)

---
+++ 3주차/문제2.java
@@ -0,0 +1,23 @@
+package 고급알고리즘;
+
+import java.util.Scanner;
+
+public class 문제2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		long result = str.charAt(0) - '0';
+		for(int i=1; i < str.length(); i++) {
+			int num = str.charAt(i) - '0';
+			if(num <= 1 || result <= 1) {
+				result += num;
+			}
+			else {
+				result *= num;
+			}
+		}
+		System.out.println(result);
+	}
+
+}
