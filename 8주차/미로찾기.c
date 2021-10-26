ce7d3756672bd8e1c5bf465c84a46c380974c9b9
이건우 <56014949+paul0734@users.noreply.github.com>
2021-10-26 19:18:39 +0900

    미로찾기
    미로찾기

--- 8주차/미로찾기.c
+++ 8주차/미로찾기.c
@@ -63,87 +63,57 @@ int isReachable(int maze[N][M])
     temp.dir = 0;
     push(&stack, temp); 
     while (!isEmpty(&stack)) {
-        temp = pop(&stack);
-        int d = temp.dir;
-        i = temp.x, j = temp.y;
-        temp.dir++;
-        push(&stack, temp);
-    if (i == fx && j == fy) {
-        printStack(&stack);
-    return TRUE;
-    }
-    if (d == 0) { //UP
-    if (
-    i
-    - 1 >= 0 && maze[
-    i
-    - 1][j] &&
-    visited[
-    i
-    - 1][j]) {
-    Data temp1;
-    temp1.x = i
-    -1;
-    temp1.y = j;
-    temp1.dir = 0;
-    visited[
-    i
-    - 1][j] = FALSE;
-    push(&stack, temp1);
-    } 
-    } else if (d == 1) { // LEFT
-    if (j
-    - 1 >= 0 && maze[
-    i][j
-    - 1] &&
-    visited[
-    i][j
-    - 1]) {
-    Data temp1;
-    temp1.x =
-    i
-    ;
-    temp1.y = j
-    -1;
-    temp1.dir = 0;
-    visited[
-    i][j
-    - 1] = FALSE;
-    push(&stack, temp1);
-    }
-    } else if (d == 2) { // DOWN
-    if (
-    i + 1 < n && maze[
-    i + 1][j] &&
-    visited[
-    i + 1][j]) {
-    Data temp1;
-    temp1.x = i+1;
-    temp1.y = j;
-    temp1.dir = 0;
-    visited[
-    i + 1][j] = FALSE;
-    push(&stack, temp1);
-    } 
-    } else if (d == 3) { // RIGHT
-    if (j + 1 < m && maze[
-    i][j + 1] &&
-    visited[
-    i][j + 1]) {
-    Data temp1;
-    temp1.x =
-    i
-    ;
-    temp1.y = j+1;
-    temp1.dir = 0;
-    visited[
-    i][j + 1] = FALSE;
-    push(&stack, temp1);
-    }
-    } else {
-    visited[temp.x][temp.y] = TRUE;
-    temp = pop(&stack);
-    }
+                temp = pop(&stack);
+                int d = temp.dir;
+                i = temp.x, j = temp.y;
+                temp.dir++;
+                push(&stack, temp);
+            if (i == fx && j == fy) {
+                printStack(&stack);
+            return TRUE;
+            }
+
+            if (d == 0) { //UP
+                if (
+                i - 1 >= 0 && maze[i- 1][j] && visited[i - 1][j]) {
+                    Data temp1;
+                    temp1.x = i-1;
+                    temp1.y = j;
+                    temp1.dir = 0;
+                    visited[i - 1][j] = FALSE;
+                    push(&stack, temp1);
+                } 
+            } else if (d == 1) { // LEFT
+                if (j - 1 >= 0 && maze[i][j - 1] && visited[i][j - 1]) {
+                    Data temp1;
+                    temp1.x = i;
+                    temp1.y = j-1;
+                    temp1.dir = 0;
+                    visited[i][j - 1] = FALSE;
+                    push(&stack, temp1);
+                }
+            } else if (d == 2) { // DOWN
+                if (i + 1 < n && maze[i + 1][j] &&visited[i + 1][j]) {
+                    Data temp1;
+                    temp1.x = i+1;
+                    temp1.y = j;
+                    temp1.dir = 0;
+                    visited[i + 1][j] = FALSE;
+                    push(&stack, temp1);
+                } 
+            } else if (d == 3) { // RIGHT
+                if (j + 1 < m && maze[i][j + 1] &&visited[i][j + 1]) {
+                    Data temp1;
+                    temp1.x = i;
+                    temp1.y = j+1;
+                    temp1.dir = 0;
+                    visited[i][j + 1] = FALSE;
+                    push(&stack, temp1);
+                }
+            } else {
+                visited[temp.x][temp.y] = TRUE;
+                temp = pop(&stack);
+            }
     }
     return FALSE;
 }
