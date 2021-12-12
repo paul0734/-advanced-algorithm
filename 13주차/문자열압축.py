6d95d6a80c93d62261f37bb5c0acb1524c469e9b
이건우 <56014949+paul0734@users.noreply.github.com>
2021-12-12 19:49:56 +0900

    카카오_문자열압축
    카카오_문자열압축

--- 13주차/문자열압축.py
+++ 13주차/문자열압축.py
@@ -1,22 +1,28 @@
-def solution(s):
-    answer=len(s)
-    for n in range(1,len(s)//2+2):
-        res=''
-        cnt=1
-        tmp=s[:n]
-        for i in range(n,len(s)+n,n):
-            if tmp == s[i:i+n]:
-               cnt+=1
-            else:
-                if cnt==1:
-                    res+=tmp
-                else:
-                    res = str(cnt) + tmp
-                tmp=s[i:i+n]
-                cnt=1
-        #print(res)
-        answer=min(answer, len(res))
-    return answer
+def solution(s): 
+    result=[] 
+    if len(s)==1:
+        return 1
+    for i in range(1, (len(s)//2)+1):
+        b = ''
+        cnt = 1
+        tmp=s[:i]
+        
+        for j in range(i, len(s), i):
+            if tmp==s[j:i+j]:
+                cnt+=1 
+            else: 
+                if cnt!=1:
+                    b = b + str(cnt)+tmp 
+                else: 
+                    b = b + tmp
+                tmp=s[j:j+i] 
+                cnt = 1 
+        if cnt!=1: 
+                b = b + str(cnt) + tmp 
+        else: b = b + tmp 
+            
+        result.append(len(b)) 
+    return min(result)
 
 
 
